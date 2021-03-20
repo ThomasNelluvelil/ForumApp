d80df5b93911abba62244e34eb987e9ac8f46238
ThomasNelluvelil <thomas.nelluvelil@mavs.uta.edu>
2021-03-20 00:00:54 -0500

    Initial commit

---
+++ app/src/main/java/com/example/forumapp/Activities/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.forumapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.forumapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
